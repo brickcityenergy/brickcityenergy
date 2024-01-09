f4c5f04f7625c11f3481eb9b2b61819e97caf474
Zachary Kraft <creativeautomaton@users.noreply.github.com>
2024-01-09 15:32:30 -0600

    changed to selfediting

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,15 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: false
-		})
-	}
+  kit: {
+    adapter: adapter(),
+    csrf: {
+      checkOrigin: false
+    }
+  },
+  preprocess: vitePreprocess()
 };
 
 export default config;
